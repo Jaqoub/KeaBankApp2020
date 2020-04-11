564f26a84b7a51737bd60ec301f237e485b106c1
altair2400 <altair2400@gmail.com>
2020-04-11 03:16:20 +0200

    slidt kode virker nu

--- app/src/main/java/com/example/keabankapp/BankInfo.java
+++ app/src/main/java/com/example/keabankapp/BankInfo.java
@@ -1,9 +1,19 @@
 package com.example.keabankapp;
 
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.View;
 
 public class BankInfo extends Activity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bank_info);
+
+
+    }
+
     @Override
     public void onClick(View v) {
 
